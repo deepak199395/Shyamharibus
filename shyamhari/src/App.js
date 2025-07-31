5fb946608ce31ebaf2ce34328802f28e2437717b
Rk Tutorial <Rjtutorial.official@gmail.com>
2025-07-31 23:58:41 +0530

    initial

--- shyamhari/src/App.js
+++ shyamhari/src/App.js
@@ -3,13 +3,14 @@ import {Routes,Route} from "react-router-dom"
 import Header from './Componants/Layout/Header'
 import Footer from './Componants/Layout/Footer'
 import Home from './Screens/Home/Home'
+import BookingScreen from './Screens/BookingScreen'
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Home/>} />
       <Route path='header' element={<Header/>} />
       <Route path='Footer' element={<Footer/>} />
-      <Route path='Footer' element={<Body/>} />
+      <Route path='BookingScreen' element={<BookingScreen/>} />
 
     </Routes>
   )
